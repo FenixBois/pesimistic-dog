8f927bb08414562f7fcf7fdd13e0beeae9ee2c00
PeanutButte7 <adam.barta007@gmail.com>
2022-12-08 22:16:33 +0100

    :sparkles: Added content getAll endpoint

--- src/server/trpc/router/content.ts
+++ src/server/trpc/router/content.ts
@@ -1,15 +1,23 @@
-import { z  } from "zod";
+import { z } from 'zod';
 
-import { protectedProcedure, router } from "../trpc";
-import { Role } from "@prisma/client";
+import { protectedProcedure, publicProcedure, router } from '../trpc';
+import { Role } from '@prisma/client';
 
 export const contentRouter = router({
-  create: protectedProcedure(Role.TEACHER, Role.DEPARTMENT_OF_ACADEMIC_AFFAIRS)
-    .input(z.object({
-      title: z.string().max(50),
-      link: z.string().max(255),
-    }))
-    .mutation(async ({ctx, input}) => {
-      return ctx.prisma.content.create({data: input});
-    })
+    create: protectedProcedure(
+        Role.TEACHER,
+        Role.DEPARTMENT_OF_ACADEMIC_AFFAIRS
+    )
+        .input(
+            z.object({
+                title: z.string().max(50),
+                link: z.string().max(255),
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            return ctx.prisma.content.create({ data: input });
+        }),
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        return ctx.prisma.content.findMany();
+    }),
 });
