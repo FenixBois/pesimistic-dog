67d59d8bbaa65d1e97601f40801b8b63b71d8289
PeanutButte7 <adam.barta007@gmail.com>
2023-01-12 22:51:20 +0100

    :sparkles: Implemented ContentPage (backend not working)

--- src/components/Content/ContentPage.tsx
+++ src/components/Content/ContentPage.tsx
@@ -1,12 +1,44 @@
 import { Button, Group, Title } from '@mantine/core';
+import { trpc } from "../../utils/trpc";
+import { ContentCard } from "components/Content";
+import { FormModal } from "../utils";
+import { useState } from "react";
+import { CreateContentForm } from "components/Forms";
 
 export function ContentPage() {
+    const utils = trpc.useContext();
+    const contents = trpc.content.getAll.useQuery().data
+    const deleteContentMutation = trpc.content.delete.useMutation({
+        onError: (error: string | undefined) => {
+            throw new Error(error);
+        },
+        onSuccess: async () => {
+            await utils.content.invalidate()
+        }
+    })
+
+    const [contentModalState, setContentModalState] = useState(false);
+
+    function removeContent(id: string) {
+        deleteContentMutation.mutate({ id: id });
+    }
+
     return (
         <>
             <Group spacing='xl'>
                 <Title>Contents</Title>
-                <Button>Add a content</Button>
+                <Button onClick={() => setContentModalState(true)}>Add a content</Button>
+                <FormModal
+                    title='Edit subject details'
+                    state={contentModalState}
+                    setState={setContentModalState}
+                >
+                    <CreateContentForm submit={() => setContentModalState(false)} />
+                </FormModal>
             </Group>
+            {contents?.map(({ id, title, link }) => (
+                <ContentCard key={id} id={id} title={title} link={link} editable removable remove={() => removeContent(id)}/>
+            ))}
         </>
     );
 }
