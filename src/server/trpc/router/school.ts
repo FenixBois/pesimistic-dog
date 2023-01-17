650a182db83520f580e6a4e8722aa77fb721f0a3
martin.macura <martin.macura@plus4u.net>
2023-01-17 08:25:34 +0100

    cleanup + school get/edit

--- src/server/trpc/router/school.ts
+++ src/server/trpc/router/school.ts
@@ -1,7 +1,26 @@
+import { protectedProcedure, publicProcedure, router } from "../trpc";
 import { z } from "zod";
-
-import { router } from "../trpc";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { convertKnownPrismaError } from "../../common/utils";
 
 export const schoolRouter = router({
-
+  edit: protectedProcedure()
+    .input(z.object({
+      id: z.string().cuid(),
+      name: z.string() // TODO max length
+    }))
+    .mutation(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.school.update({ where: { id: input.id }, data: { name: input.name } });
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+          convertKnownPrismaError(e);
+        }
+        throw e;
+      }
+    }),
+  get: publicProcedure
+    .query(async ({ ctx }) => {
+      return ctx.prisma.school.findFirst();
+    })
 });
